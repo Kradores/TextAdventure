9afb19c0a8f9f276f13441183c4e56b69a9abd2f
Alexandru Noroc <kradores10@gmail.com>
2019-02-27 22:15:33 +0200

    Adding state - textArea and array of states and a way to assign to one state next posible states

--- Text101/Assets/AdventureGame.cs
+++ Text101/Assets/AdventureGame.cs
@@ -6,11 +6,18 @@ using UnityEngine.UI;
 public class AdventureGame : MonoBehaviour
 {
 	[SerializeField] Text textComponent;
+	[SerializeField] State startingState;
+
+	int[] oddNumbers = { 1, 3, 5, 7, 9 };
+
+	State state;
 
 	// Start is called before the first frame update
 	void Start()
 	{
-		textComponent.text = "Hello World!";
+		state = startingState;
+		textComponent.text = state.GetStateStory();
+		Debug.Log(oddNumbers[3]);
 	}
 
 	// Update is called once per frame
